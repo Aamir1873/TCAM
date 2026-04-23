5e595195059c2447d1314329dc18740406271f99
Aamir1873 <149040255+Aamir1873@users.noreply.github.com>
2026-04-23 16:00:23 +0300

    Initial Commit

---
+++ TCAM/TCAMApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCAMApp.swift
+//  TCAM
+//
+//  Created by Aamir Abdul Kader on 23/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct TCAMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
